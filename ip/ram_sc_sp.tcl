164e9f9b28ee1fe51feceddbf0069715f868f7ee
Mike Y. Tsivinsky <m_tsiv@mail.ru>
2017-04-19 19:15:16 +0700

    add new ip cfg support
    In old version make check and create only *.xcix ip containers
    In new version I remake this feature and now make check and create only ip directory,
    but ip creation tool can work with *.xci and *.xcix ip representations.

--- ip/ram_sc_sp.tcl
+++ ip/ram_sc_sp.tcl
@@ -9,7 +9,7 @@ set_property CONFIG.Memory_Type       Single_Port_RAM    [get_ips $ipCoreName]
 set_property CONFIG.Algorithm         Minimum_Area       [get_ips $ipCoreName]
 set_property CONFIG.Operating_Mode_A  WRITE_FIRST        [get_ips $ipCoreName]
 set_property CONFIG.Pipeline_Stages   0                  [get_ips $ipCoreName]
-set_property CONFIG.Port_A_Clock      100                [get_ips $ipCoreName]
+#set_property CONFIG.Port_A_Clock      100                [get_ips $ipCoreName]
 set_property CONFIG.Reset_Type        SYNC               [get_ips $ipCoreName]
 set_property CONFIG.Enable_A          Use_ENA_Pin        [get_ips $ipCoreName]
 
